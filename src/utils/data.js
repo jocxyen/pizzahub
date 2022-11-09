fe595b9ac82adc7f87096536333ec2bbd2a79419
Jocxyen <jocxyen@gmail.com>
2022-11-09 19:03:45 +0800

    Seup firebase

---
+++ src/utils/data.js
@@ -0,0 +1,22 @@
+export const categories = [
+  {
+    id: 1,
+    name: 'Pizzas',
+    urlParamName: 'pizzas',
+  },
+  {
+    id: 2,
+    name: 'Sides',
+    urlParamName: 'sides',
+  },
+  {
+    id: 3,
+    name: 'Desserts',
+    urlParamName: 'desserts',
+  },
+  {
+    id: 4,
+    name: 'Drinks & Shakes',
+    urlParamName: 'drinks',
+  },
+];
